4a61afe0b1bca3e1049e2004759aa47385bcd1d6
mohamedaminakdim09-art <mohamedaminakdim09@gmail.com>
2026-06-12 13:44:43 +0200

    new file

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const { Client, GatewayIntentBits } = require('discord.js');
 const cors = require('cors');
 const app = express();
 
-app.use(cors());
+// HADI HYA LI ZDTLIK - CORS M3A LINK DIALK
+app.use(cors({
+  origin: ['https://oaminnox-dev.netlify.app', 'http://localhost:5500']
+}));
 app.use(express.json());
 
 const client = new Client({ 
@@ -27,8 +30,10 @@ app.post('/verify', (req, res) => {
   const index = codes.indexOf(code.toUpperCase());
   if(index !== -1) {
     codes.splice(index, 1);
+    console.log(`Code ${code} tverifica ✅`);
     res.json({ valid: true });
   } else {
+    console.log(`Code ghalat: ${code}`);
     res.json({ valid: false });
   }
 });
@@ -43,13 +48,15 @@ client.on('messageCreate', message => {
   if (message.content === '!getcode') {
     const code = Math.random().toString(36).substring(2, 8).toUpperCase();
     codes.push(code);
-    message.author.send(`**Code dialk:** \`${code}\`\nSali7 l 10 min`).catch(() => {
-      message.reply('❌ 7l DM dialk');
+    message.author.send(`**🔑 Code dialk:** \`${code}\`\n**Sali7 l:** 10 min\n\nSir l site: https://oaminnox-dev.netlify.app/`).catch(() => {
+      message.reply('❌ 7l DM dialk a bro');
     });
     message.reply('✅ Sifft lik code f DM');
+    console.log(`Code jdid: ${code}`);
     
     setTimeout(() => {
       codes = codes.filter(c => c !== code);
+      console.log(`Code ${code} mcha 3lih l7al`);
     }, 10 * 60 * 1000);
   }
 });
