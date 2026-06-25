56969ecb70839ee1062e1eabe9ec659a3c5c9a29
mohamedaminakdim09-art <mohamedaminakdim09@gmail.com>
2026-06-25 18:21:10 +0200

    sss

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,19 +1,17 @@
 require('dotenv').config();
 const { REST, Routes } = require('discord.js');
 
+console.log('TOKEN:', !!process.env.TOKEN);
+console.log('CLIENT_ID:', !!process.env.CLIENT_ID);
+
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
     try {
         console.log('Kanmse7o Global Commands l9dam...');
-        
-        // Hadi katmse7 ga3 lcommands lglobal men Discord kamel
         await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: [] });
         console.log('✅ Global Commands tmse7o kamlin!');
-        
-        console.log('Ma3endnach Guild Commands daba, safi bot n9i');
-        
     } catch (error) {
-        console.error(error);
+        console.error('❌ Error:', error.message);
     }
 })();
